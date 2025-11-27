b9da6f151bffe3ab2040feba56df0c70b6280159
jaehe0130 <165889783+jaehe0130@users.noreply.github.com>
2025-11-27 12:23:33 +0900

    Fix duplicate page_icon in set_page_config

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", page_icon="❄️", page_title="PowderGuide", page_icon="❄️")
+st.set_page_config(layout="wide",  page_title="PowderGuide", page_icon="❄️")
 
 # 버튼 스타일
 st.markdown("""
